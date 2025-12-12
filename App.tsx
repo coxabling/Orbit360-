bdc1ae96c48a324bd445670f0433ac76fafebc73
coxabling <coxabling@users.noreply.github.com>
2025-12-12 00:30:54 +0000

    feat: Embed YouTube video playlist
    Replaces the single embedded YouTube video with a link to a video series playlist. This will allow users to easily view multiple showcase videos of the Orbit360 Motion experience.

--- App.tsx
+++ App.tsx
@@ -740,10 +740,11 @@ export default function App() {
                     <div className="relative pb-[56.25%] h-0">
                         <iframe 
                             className="absolute top-0 left-0 w-full h-full"
-                            src="https://www.youtube.com/embed/e-_9LXYkQXE?rel=0" 
-                            title="Orbit360 Moments in Motion"
+                            src="https://www.youtube.com/embed/videoseries?si=tN7p-_cM_DsYCaYo&amp;list=PLe1uGkO3QZS1FaKgtCpQnWny5bdfaTg7b" 
+                            title="YouTube video player"
                             frameBorder="0" 
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                            referrerPolicy="strict-origin-when-cross-origin"
                             allowFullScreen>
                         </iframe>
                     </div>
